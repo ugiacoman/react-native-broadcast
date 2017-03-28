36b5385b4539dc7e468295eb24158dc70420c659
Ulises Giacoman <ulises.giacoman@forrent.com>
2017-03-28 10:03:40 -0400

    Add req for permissions and initial Broadcast module

--- index.ios.js
+++ index.ios.js
@@ -9,10 +9,55 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NativeModules
 } from 'react-native';
+const Permissions = require('react-native-permissions');
+
 
 export default class RNBroadcast extends Component {
+  _requestCamera() {
+    Permissions.requestPermission('camera')
+      .then(response => {
+        //returns once the user has chosen to 'allow' or to 'not allow' access
+        //response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        console.log("camera response after req", response)
+      });
+  }
+  _requestMicrophone() {
+    Permissions.requestPermission('microphone')
+      .then(response => {
+        //returns once the user has chosen to 'allow' or to 'not allow' access
+        //response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        console.log("microphone response after req", response)
+      });
+  }
+
+  componentDidMount() {
+    Permissions.getPermissionStatus('camera')
+      .then(response => {
+        //response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        console.log('permissions response', response)
+        if (response === 'authorized' ){
+          console.log("camera authorized")
+        } else {
+          this._requestCamera()
+        }
+      });
+    Permissions.getPermissionStatus('microphone')
+      .then(response => {
+        //response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+        console.log('micro response', response)
+        if (response === 'authorized' ){
+          console.log("yay")
+        } else {
+          this._requestMicrophone()
+        }
+      });
+    var Broadcast = NativeModules.Broadcast
+    Broadcast.addEvent("boby", "sanders")
+  }
+
   render() {
     return (
       <View style={styles.container}>
