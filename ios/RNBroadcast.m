5bc2010d76c9f737d6796eb5b4ad740470a02e54
Ulises Giacoman <ulises.giacoman@forrent.com>
2017-04-03 16:46:46 -0400

    Add preview

--- ios/RNBroadcast.m
+++ ios/RNBroadcast.m
@@ -1,17 +1,18 @@
 
 #import "LFLivePreview.h"
 #import <React/RCTViewManager.h>
+#import <UIKit/UIKit.h>
 
 @interface RNBroadcastViewManager : RCTViewManager
 @end
 
 @implementation RNBroadcastViewManager
-
 RCT_EXPORT_MODULE()
 
 - (UIView *)view
 {
-    return [[LFLivePreview alloc] initWithFrame:self.view.bounds];
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    return [[LFLivePreview alloc] initWithFrame:screen];
 }
 
 @end
