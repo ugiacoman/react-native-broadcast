d815a7636ea4c6ef11bb8cd593833874e0daa9e1
Adrián Barrio Andrés <statickidz@gmail.com>
2018-04-09 23:20:17 +0200

    Fix supertype overrides
    node_modules/react-native-broadcast/android/src/main/java/com/reactlibrary/RNBroadcastPackage.java:23: error: method does not override or implement a method from a supertype
        @Override
        ^
    Note: Some input files use or override a deprecated API.
    Note: Recompile with -Xlint:deprecation for details.

--- android/src/main/java/com/reactlibrary/RNBroadcastPackage.java
+++ android/src/main/java/com/reactlibrary/RNBroadcastPackage.java
@@ -15,17 +15,14 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 public class RNBroadcastPackage implements ReactPackage {
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new RNBroadcastViewManager()
